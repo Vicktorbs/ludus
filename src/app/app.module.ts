a13a16a6f16490dab95b75ffa05819043c931a69
Victor Bolaños Solís <viktorb.132@gmail.com>
2019-05-03 13:48:59 -0500

    Log y registro, interfaz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UserlogComponent } from './userlog/userlog.component';
+import { UserregisterComponent } from './userregister/userregister.component';
+import { NavComponent } from './nav/nav.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserlogComponent,
+    UserregisterComponent,
+    NavComponent
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule,
     AppRoutingModule
   ],
